b94296b2fbcc5b8e8d9feff5c5b2798cc7033039
NettoJM <nettojm86@gmail.com>
2023-04-12 19:46:20 -0300

    Add descriptive names to http statuses

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,12 +1,15 @@
 const { loginService } = require('../services');
 
+const HTTP_OK_STATUS = 200;
+const HTTP_BAD_REQUEST_STATUS = 400;
+
 const signin = async (req, res) => {
   try {
     const { email, password } = req.body;
     const login = await loginService.authenticate(email, password);
-    return res.status(200).json({ token: login });
+    return res.status(HTTP_OK_STATUS).json({ token: login });
   } catch ({ message }) {
-    return res.status(400).json({ message });
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({ message });
   }
 };
 
