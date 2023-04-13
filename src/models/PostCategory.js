1c1ac7eeaa1886394af20d32d63bbd307bd14644
NettoJM <nettojm86@gmail.com>
2023-04-13 20:59:22 -0300

    Add PostCategory model and its N:N relation with the "users" and "blog_posts" tables

---
+++ src/models/PostCategory.js
@@ -0,0 +1,37 @@
+const PostCategorySchema = (sequelize, DataTypes) => {
+  const PostCategoryTable = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+  },
+    {
+      tableName: 'posts_categories',
+      underscored: true,
+      timestamps: false,
+    });
+
+  PostCategoryTable.associate = ({ BlogPost, Category }) => {
+    Category.belongsToMany(BlogPost, {
+      as: 'posts',
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      through: PostCategoryTable
+    });
+
+    BlogPost.belongsToMany(Category, {
+      as: 'categories',
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+      through: PostCategoryTable
+    });
+  }
+
+  return PostCategoryTable;
+};
+
+module.exports = PostCategorySchema;
