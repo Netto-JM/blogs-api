2321909858630f95f74ac55906a2268b877116a1
NettoJM <nettojm86@gmail.com>
2023-04-18 17:07:40 -0300

    Add "getPostData" helper to extract posts data

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,18 +1,22 @@
 const { BlogPost, User, Category } = require('../models');
 
+const getPostData = (post) => {
+  const postData = {
+    ...post.dataValues,
+    user: post.dataValues.user.dataValues,
+    categories: post.dataValues.categories.map((category) => category.dataValues),
+  };
+
+  return postData;
+};
+
 const findAll = async () => {
   const data = await BlogPost.findAll({
     include: [{ model: User, as: 'user', attributes: { exclude: ['password'] } }, 
     { model: Category, as: 'categories' }],
   });
-  const posts = data.map((post) => {
-    const myPost = {
-      ...post.dataValues,
-      user: post.dataValues.user.dataValues,
-      categories: post.dataValues.categories.map((category) => category.dataValues),
-    };
-    return myPost;
-  });
+  
+  const posts = data.map(getPostData);
   return posts;
 };
 
@@ -25,12 +29,7 @@ const findById = async (id) => {
   
   if (!data) throw new Error('Post does not exist');
 
-  const post = {
-    ...data.dataValues,
-    user: data.dataValues.user.dataValues,
-    categories: data.dataValues.categories.map((category) => category.dataValues),
-  };
-
+  const post = getPostData(data);
   return post;
 };
 
